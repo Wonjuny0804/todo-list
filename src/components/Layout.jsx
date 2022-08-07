e5deaf333c7a5695d3271dc869d2936ddf1829ee
Wonjun <wonwonjun@gmail.com>
2022-08-07 23:20:55 +0900

    Create new bg layout for todos

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,58 +1,22 @@
-import React from 'react'
-import Header from './Header'
-import Input from './Input'
-import TodoItem from './TodoItem'
-import styled from 'styled-components';
-import BackgroundImg from '../images/phone_img2.webp'
+import React from "react";
+import styled from "styled-components";
+import TodoList from "./TodoList";
 
-const LayoutBox = styled.div`
-    width: 100%;
-    height: 100vh;
-    `
-const Container = styled.div`
-    //핸드폰 이미지
-    //background-image: url(${BackgroundImg});
-    //background: cover;
-    //background-repeat: no-repeat;
-    //background-position: center; //이미지 가운데로
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px pink solid;
-    flex-direction: column;
-`
-const PhoneSize = styled.div`
-    background-color: #fdefb8;
-    width: 500px;
-    height: 800px;
-    border-radius: 15px;
-    display: flex;
-    //align-items: center;
-    border: 1px pink solid;
-    flex-direction: column;
-`
-const DateInput = styled.div`
-    display: flex;
-    margin: -30px auto 10px auto;
-    padding: 2rem;
-    border-bottom: 1px solid gray;
-`
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Layout = () => {
-    return (
-        <LayoutBox>
-            <Container>
-                <PhoneSize>
-                    <DateInput>
-                        <Header />
-                        <Input />
-                    </DateInput>
-                </PhoneSize>
-            </Container>
-        </LayoutBox>
-    )
-}
+  return (
+    <Wrapper>
+      <TodoList />
+    </Wrapper>
+  );
+};
 
-export default Layout
+export default Layout;
